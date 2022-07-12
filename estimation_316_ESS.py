48289ef821bf7e3a624a13d8621c4cbb8a83f558
Thomas <NWT@nrc.gov>
2022-07-12 16:03:43 -0400

    initial commit

---
+++ estimation_316_ESS.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 13:22:42 2022
+
+@author: NWT
+"""
+
+import sys; sys.path.insert(0, '../../');  import PEUQSE as PEUQSE
+import PEUQSE.UserInput as UserInput
+
+if __name__ == "__main__":
+    import data_316 #Just a simple example. The user can also put the values in directly into the runfile or extract from a csv, for example.
+    import function_316 #Simple example.
+        
+    #Provide the observed X values and Y values and uncertainties -- all should be arrays or lists with nesting like [[1,2,3]] or [[1,2,3,4],[4,5,6,6]]
+    UserInput.responses['responses_abscissa'] = data_316.distances
+    UserInput.responses['responses_observed'] = data_316.concentrations
+    UserInput.responses['responses_observed_uncertainties'] = data_316.errors
+   
+    #Optional: provide labels for the responses axes and parameter names.
+    UserInput.simulated_response_plot_settings['x_label'] = 'distance (um)'
+    UserInput.simulated_response_plot_settings['y_label'] = r'$Concentration (wt\%)$'
+    UserInput.model['parameterNamesAndMathTypeExpressionsDict'] = {'a':'a','b':'b'}
+    
+    #Provide the prior distribution and uncertainties of the individual parameters.
+    UserInput.model['InputParameterPriorValues'] = [4.2*10**-19, 16.825] #prior expected values for a and b
+    UserInput.model['InputParametersPriorValuesUncertainties'] = [1*10**-19, 0.5] #required. #If user wants to use a prior with covariance, then this must be a 2D array/ list. To assume no covariance, a 1D
+    #UserInput.model['InputParameterInitialGuess'] = [150,400] #Can optionally change the initial guess to be different from prior means.
+
+    #Provide a function that returns simulated values -- must of the same form as observed values, should be arrays or lists with nesting like [[1,2,3]] or [[1,2,3,4],[4,5,6,6]]
+    UserInput.model['simulateByInputParametersOnlyFunction'] = function_316.simulation_function_wrapper #This must simulate with *only* the parameters listed above, and no other arguments.
+
+    #mcmc length should typically be on the order of 10,000 per parameter. By default, the burn in will be the first 10% of the mcmc length.
+    UserInput.parameter_estimation_settings['mcmc_length'] = 100 #10000 is the default.
+    UserInput.parameter_estimation_settings['mcmc_walkerInitialDistribution'] = 'identical'
+    #After filinlg the variables of the UserInput, now we make a 'parameter_estimation' object from it.
+    PE_object = PEUQSE.parameter_estimation(UserInput)
+    
+    #Now we can do the mcmc!
+    PE_object.doEnsembleSliceSampling()
+    #Another option would be PE_object.doEnsembleSliceSampling(), one can also do grid search or an astroidal distribution search.
+    
+    #Finally, create all plots!
+    
+    PE_object.createAllPlots()
+    #The createAllPlots function calls each of the below functions so that the user does not have to.    
+    #    PE_object.makeHistogramsForEachParameter()    
+    #    PE_object.makeSamplingScatterMatrixPlot()
+    #    PE_object.createSimulatedResponsesPlots()
+
+""" 
+    #########Optional example of saving and loading PE_objects after running the mcmc.
+    #########This feature requires having dill installed (pip install dill, https://pypi.org/project/dill/)
+    try:
+        import dill
+        dillModuleExists = True
+    except:
+        dillModuleExists = False
+
+    
+    #Optionally, one can save a PE_object for later,if the dill module has been installed.
+    if dillModuleExists == True: 
+        PE_object.save_to_dill("PE_object_00a0")
+        #to load a PE_object after some time, first one has to put (any) UserInput to create a PE_object, then to load from file.
+        
+        
+        #Normally, we would do the loading and plotting in another python file, but for this example the syntax is being demonstrated below within the same file.
+        PE_object2 = PEUQSE.parameter_estimation(UserInput)
+        PE_object2 = PE_object2.load_from_dill("PE_object_00a0")
+        PE_object2.createAllPlots()
+"""
---
+++ function_316.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 12 13:27:52 2022
+
+@author: NWT
+"""
+
+import numpy as np
+import math
+import scipy
+
+
+#To use PEUQSE, you can have a function, but you also need to make a function wrapper that takes *only* the parameters as a single vector.
+def simulationFunction(x,a,b): #here x is a scalar or an array and "a" and "b" are constants for the equation.
+    C0Cr = b # 16.825
+    t = 3000
+    x =np.array(x)
+    y =  C0Cr*scipy.special.erf(
+        (x*10**-6)/(2*(a**(1/2))*t)
+        ) #This is the same as d = (t-a)**2 + b
+    #print("line 9", y)
+    return y
+
+
+
+#Now we will make a wrapper for the simulation function, since PEUQSE needs that.
+x_values_for_data = []  #This is just initializing the global value to avoid confusion (see below)
+#Now to populate the global variable.
+import data_316 #I am using an import to show we can use the x_values associated with the observed data. Our wrapper should take only parameters and not x values.
+x_values_for_data = data_316.distances
+def simulation_function_wrapper(parametersArray):#this has a and b in it.
+    global x_values_for_data  #It is a good idea to create a global variable to pass in the x_values for your simulation.
+    a_given = parametersArray[0] #a "given" just means this wrapper will simulate using whatever a value it receives.
+    b_given = parametersArray[1] #b "given" just means this wrapper will simulate using whatever b value it receives.
+    y = simulationFunction(x_values_for_data, a_given, b_given)  #an alternatie simpler syntax to unpack the parameters would be: simulationFunction(x_values_for_data, *parametersArray) 
+    return y
+    
