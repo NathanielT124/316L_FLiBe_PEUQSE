ee7db53f48b795c5c4fe2a7b6dc251e3856f5c6b
NathanielT124 <nwt@nrc.gov>
2022-07-20 13:10:23 -0400

    Better graphs of uncertainty

--- Deff_values_against_uncertainty.py
+++ Deff_values_against_uncertainty.py
@@ -9,40 +9,55 @@ It graphs MAP and MU_AP values, as well as standard deviations
 import matplotlib.pyplot as plt
 import math
 
-map_values = [-18.37662834,-18.36896044,-18.0852596,-17.95542376] # ,-17.95128962]
-mu_ap_values = [-18.37667027,-18.36823056,-18.08384853,-17.9557197] # ,-14.26723996]
-std_devs = [0.00093476,0.00948315,0.05376846,0.06246781] # ,5.86600188]
-uncertainties = [-3.0,-2.0,-1.0,0.0] # ,1.0]
+map_values = [-18.37662834,-18.36896044,-18.0852596,-17.95542376,-17.945498]
+mu_ap_values = [-18.37667027,-18.36823056,-18.08384853,-17.9557197,-17.95503955]
+std_devs = [0.00093476,0.00948315,0.05376846,0.06246781,0.0626771]
+for i in range(5):
+    map_values[i] = 10**map_values[i]
+    mu_ap_values[i] = 10**mu_ap_values[i]
+
+uncertainties = [0.001, 0.01, 0.1, 1.0, 10.0]
 literature_values = []
-for i in range(4): # 5):
-    literature_values.append(math.log10(4.2*10**-19))
+for i in range(5):
+    literature_values.append(4.2*10**-19)
 
 # Create a graph of MAP values against uncertainty
 plt.figure(0)
 plt.plot(uncertainties, map_values, 'ko-')
 plt.plot(uncertainties, literature_values, 'g--')
-plt.xlim([-3.1, 1.1])
-plt.xlabel("Log base10 of uncertainty")
-plt.ylabel("Log base10 of diffusion coefficient")
-plt.legend(["PEUQSE values","Experimental value"])
-plt.title("MAP values vs log base10 of uncertainty")
+# Axis labels
+plt.xlabel("Uncertainty")
+plt.ylabel("Diffusion coefficient")
+# Axis log scale
+plt.xscale("log")
+plt.yscale("log")
+plt.legend(["Bayesian Estimate","Zheng 2016"])
+plt.title("MAP diffusion coefficient vs uncertainty")
+plt.grid(True, which="both")
 plt.show()
 
+# Create a graph of MU_AP values against uncertainty
 plt.figure(1)
 plt.plot(uncertainties, mu_ap_values, 'ko-')
 plt.plot(uncertainties, literature_values, 'g--')
-plt.xlim([-3.1, 1.1])
-plt.xlabel("Log base10 of uncertainty")
+plt.xlabel("Uncertainty")
+# Axis labels
 plt.ylabel("Log base10 of diffusion coefficient")
-plt.legend(["PEUQSE values","Experimental value"])
-plt.title("MU_AP values vs log base10 of uncertainty")
+plt.xscale("log")
+plt.yscale("log")
+plt.legend(["Bayesian Estimate","Zheng 2016"])
+plt.title("MU_AP MAP Log base 10 of diffusion coefficient vs uncertainty")
+plt.grid(True, which="both")
 plt.show()
 
+# Create a graph of standard deviations against uncertainty
 plt.figure(2)
 plt.plot(uncertainties, std_devs, 'ko-')
-plt.xlim([-3.1, 1.1])
-plt.xlabel("Log base10 of uncertainty")
+plt.xlabel("Uncertainty")
 plt.ylabel("Standard deviation")
-plt.legend(["PEUQSE values","Experimental value"])
-plt.title("Standard deviation vs log base10 of uncertainty")
+# Axis labels
+plt.xscale("log")
+plt.legend(["Bayesian Estimate","Zheng 2016"])
+plt.title("Standard deviation of Log base 10 of diffusion coefficient vs uncertainty")
+plt.grid(True, which="both")
 plt.show()
